f0083a6b5d7201d45390db61d8ee0692db9fe32f
Kevin Murphy <midimurphdesigns@gmail.com>
2018-02-21 10:39:51 -0700

    added person objects and a person array

--- afternoon-1.js
+++ afternoon-1.js
@@ -31,4 +31,30 @@ const mealObj = {
     ]
 };
 
-console.log(mealObj.meal[3]);
+// console.log(mealObj.meal[3]);
+
+//////////////////////
+
+const person1 = {
+    name: 'Bill',
+    jobTitle: 'Janitor'
+};
+
+const person2 = {
+    name: 'Ted',
+    jobTitle: 'pilot'
+};
+
+const person3 = {
+    name: 'Alfred',
+    jobTitle: 'Web Dev'
+};
+
+const person4 = {
+    name: 'Jenny',
+    jobTitle: 'Manager'
+};
+
+const personArray = [person1, person2, person3, person4];
+
+personArray.forEach(obj => console.log(`${obj.name}: ${obj.jobTitle}`));
